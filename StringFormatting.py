4e1313c0128e9db741e7324f118f1f9d389b7a13
Wayne Harrison Jnr <WayneJnr@planetwayne.com>
2020-11-05 10:23:08 +0000

    String Formatting
    How to format strings and clean up the code by using the formatting tools built into python.

---
+++ StringFormatting.py
@@ -0,0 +1,9 @@
+print('Hello' + ' world')
+name = 'Wayne'
+place = 'Town'
+time = '11AM'
+food = 'Sausage rolls'
+print('Hello ' + name + ', you are invited to ' + place + ' at ' + time
+      + '. There will be ' + food + '.')
+print ('Hello %s, you are invited to %s at %s. There will be %s.'
+       %(name, place, time, food))
