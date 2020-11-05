c119138d3bb1d97b797f4bfc9ca8c139668eaefb
Wayne Harrison Jnr <WayneJnr@planetwayne.com>
2020-11-05 10:24:52 +0000

    Update StringFormatting.py

--- StringFormatting.py
+++ StringFormatting.py
@@ -5,5 +5,5 @@ time = '11AM'
 food = 'Sausage rolls'
 print('Hello ' + name + ', you are invited to ' + place + ' at ' + time
       + '. There will be ' + food + '.')
-print ('Hello %s, you are invited to %s at %s. There will be %s.'
-       %(name, place, time, food))
+print ('Hello %s, you are invited to %s at %s. There will be %s.' #The %s acts as a place holder within the string, the variables can be inserted 
+       %(name, place, time, food))                                #after in order to help keep code clean when compared to the line above.
